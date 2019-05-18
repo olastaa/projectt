abd78516cca84451219ed2a8d9017b7bce7a8b5d
Lukass777 <50771168+Lukass777@users.noreply.github.com>
2019-05-18 16:02:22 +0200

    Add files via upload

--- strona1.php
+++ strona1.php
@@ -16,6 +16,11 @@ if(!isset($_SESSION['zalogowany']))
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <title>ProjectTracker</title>
+         <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    {% block stylesheets %}{% endblock %}
+    <title>{% block title %}Witaj na Uczelni!{% endblock %}</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
     </head>
     <body>
         <header>
@@ -23,6 +28,24 @@ if(!isset($_SESSION['zalogowany']))
                 <a class="navbar-brand" href="#"><img src="alien_head.gif" width="30" height="30" alt="">Projekty</a>
             <a class="navbar-brand" href="#">Klient</a>
             </nav>
+             <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Projekty</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Klienci <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pracownicy</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+      <main class="container-fluid">
+  {% block body %}{% endblock %}
         </header>
         <table>
                     <tr><th>ID</th>
