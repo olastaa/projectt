87b5d716cce06bfd09912cbd58f4dfe8535f7820
Lukass777 <50771168+Lukass777@users.noreply.github.com>
2019-05-23 21:04:16 +0200

    Add files via upload
    drobne zmiany i uporządkowanie kodu.
    W Zaloguj.php dodalem do SELECT po prostu nazwy kolejnych kolumn i działa wyciąganie z bazy.

--- strona1.php
+++ strona1.php
@@ -15,18 +15,20 @@ if(!isset($_SESSION['zalogowany']))
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <title>ProjectTracker</title>
+        <title>Project Tracker - Aleksandra i Łukasz</title>
          <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    {% block stylesheets %}{% endblock %}
-    <title>{% block title %}Witaj na Uczelni!{% endblock %}</title>
+    <title>Witaj w aplikacji</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
     </head>
     <body>
         <header>
-            <nav class="navbar bg-primary">
+            <nav class="navbar bg-primary navbar-light">
                 <a class="navbar-brand" href="#"><img src="alien_head.gif" width="30" height="30" alt="">Projekty</a>
             <a class="navbar-brand" href="#">Klient</a>
+            <a class="navbar-brand" href="#">Pracownicy</a>
+            <a class="navbar-brand" href="#">Zadania</a>
+            <a class="navbar-brand" href="logout.php"><input type="button" value="Wylogowanie"</a>
             </nav>
              <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Projekty</a>
@@ -47,39 +49,22 @@ if(!isset($_SESSION['zalogowany']))
     </ul>
   </div>
 </nav>
-      <main class="container-fluid">
-  {% block body %}{% endblock %}
+      <main class="container-fluid" {% block body %}{% endblock %}>
         </header>
-        <table>
-                    <tr><th>ID</th>
-                    <th>Klient</th>
-                    <th>Opis</th>
-                    </tr>
-        <?php
-        echo "<p>Witaj ".$_SESSION['user'].'! <a href="logout.php"><input type="submit" value="Wylogowanie!"</a></p>';       
-        echo "<p><b>Projekt</b>:".$_SESSION['Projekt'];
-        echo " | <b>Klient</b>:".$_SESSION['Klient'];
-        echo " | <b>Pracownik</b>:".$_SESSION['Pracownik']."</p>";
-        
-        echo "<p><b>Status</b>:".$_SESSION['Status']."</p>";  
+        <h2>Projekty w bazie</h2>
+        <br />
 
-        $conn = mysqli_connect("localhost", "root", "");  
-        $sql = "SELECT ID, user, email from ID_Pracownicy";
-        $result = $conn-> query($sql);
-        
-        if ($result-> num_rows > 0) {
-        while ($row = $result-> fetch_assoc()) {
-        echo "<tr><td>". $row["ID"] . "</td><td>". $row["user"] ."</td><td>". $row["email"] ."</td></tr>";
-    }
+        <?php
+        echo "<table>";
+        echo "<p>Witaj ".$_SESSION['user']."!</p>";
+        echo "<p><b>Projekt</b>: ".$_SESSION['Projekt'];
+        echo " | <b>Klient</b>: ".$_SESSION['Klient'];
+        echo " | <b>Pracownik</b>: ".$_SESSION['Pracownik']."</p>";
         echo "</table>";
-}
-else {
-echo "0 result";
-}
-$conn-> close();
+        
 ?>
-</table>
         <br /><br />
         <a href="dodajnowyp.php"><input type="submit" value="Dodaj nowy Projekt!"></a>
+        <br /><br />
     </body>
 </html>
--- zaloguj.php
+++ zaloguj.php
@@ -14,7 +14,7 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 
 if ($polaczenie->connect_errno!=0)
 {
- echo "Error: ".$polaczenie->connect_errno . " Opis: ". $polaczenie->conect_error;
+ echo "Error: ".$polaczenie->connect_errno . " Opis: ". $polaczenie->connect_error;
 }
  else {
     $login = $_POST['login'];
@@ -26,38 +26,42 @@ if ($polaczenie->connect_errno!=0)
     
     /* zabezpieczenie przed wtryskiwaniem obcego mysql: */
     $login = htmlentities($login, ENT_QUOTES, "UTF-8");
-    $haslo = htmlentities($haslo, ENT_QUOTES, "UTF-8");
     
     if($rezultat = @$polaczenie->query(
-            sprintf("SELECT * FROM pracownicy WHERE user='%s' AND pass='%s'",
-    mysqli_real_escape_string($polaczenie,$login),
-    mysqli_real_escape_string($polaczenie,$haslo))))
+    sprintf("SELECT * FROM pracownicy, projekty, klienci WHERE user='%s'",
+    mysqli_real_escape_string($polaczenie,$login))))
     {
       $ilu_userow = $rezultat->num_rows;
       if($ilu_userow>0)
       {
-          $_SESSION['zalogowany'] = true;
-          
           $wiersz = $rezultat->fetch_assoc();
-          $_SESSION['id'] = $wiersz['id'];
-          $_SESSION['user'] = $wiersz['user'];
-          $_SESSION['Projekt'] = $wiersz['Projekt'];
-          $_SESSION['Klient'] = $wiersz['Klient'];
-          $_SESSION['Pracownik'] = $wiersz['Pracownik'];
-          $_SESSION['Status'] = $wiersz['Status'];
           
-          unset($_SESSION['blad']);
-          $rezultat->free_result();
-          
-          /*przekierowanie do innego dokumnetu po logowaniu*/
-          header('Location: strona1.php');
-          
+          if(password_verify($haslo, $wiersz['pass']))
+          {
+                $_SESSION['zalogowany'] = true;
+                $_SESSION['user'] = $wiersz['user'];
+                $_SESSION['Projekt'] = $wiersz['Projekt'];
+                $_SESSION['Klient'] = $wiersz['Klient'];
+                $_SESSION['Pracownik'] = $wiersz['Pracownik'];
+                $_SESSION['Zadanie'] = $wiersz['Zadanie'];
+
+                unset($_SESSION['blad']);
+                $rezultat->free_result();
+                /*przekierowanie do innego dokumnetu po logowaniu*/
+                header('Location: strona1.php');
+          }
+          else 
+          {
+          $_SESSION['blad'] = '<span style="color:red">Nieprawidlowe hasło!</span>';
+          header('Location: index.php');
+          }
       } else {
           $_SESSION['blad'] = '<span style="color:red">Nieprawidlowy login lub hasło!</span>';
           header('Location: index.php');
     }
-    $polaczenie->close();
+
 }
+   $polaczenie->close();
  }
 /* 
 sprawdzenie, czy skrypt poprawnie czyta dane:
